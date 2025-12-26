c433b4b41d9eb397dc626a323a7ead610d373daf
Csaba Szilagyi <mognoose@gmail.com>
2025-12-26 23:48:12 +0200

    fix: minor layout issue

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -5,10 +5,12 @@
             <h1>{{ recipe.title }}</h1>
             <p>{{ recipe.description }}</p>
             <img v-if="recipe.imageUrl" :src="recipe.imageUrl" alt="Recipe Image" />
-            <ul>
-                <li v-for="ingredient in recipe.ingredients" :key="ingredient">{{ ingredient }}</li>
-            </ul>
-            <p>{{ recipe.instructions }}</p>
+            <div class="content">
+                <ul>
+                    <li v-for="ingredient in recipe.ingredients" :key="ingredient">{{ ingredient }}</li>
+                </ul>
+                <p>{{ recipe.instructions }}</p>
+            </div>
         </div>
         <div v-else-if="error">Error loading recipe</div>
     </article>
@@ -42,15 +44,41 @@ const { data: recipe, pending, error } = await useFetch<Recipe>(`/api/recipes/${
             border-radius: 8px;
         }
 
-        ul {
-            margin: 1rem 0;
-            padding-left: 1.5rem;
+        .content {
+            margin-top: 1.5rem;
+            display: flex;
+
+            ul {
+                white-space: nowrap;
+                margin: 1rem 0;
+                padding-left: 1.5rem;
+            }
+            
+            p {
+                padding-left: 2rem;
+                margin-left: 2rem;
+                border-left: 1px solid var(--pico-secondary-border);
+                margin-top: 1rem;
+                line-height: 1.6;
+                white-space: break-spaces;
+            }
         }
 
-        p {
-            margin-top: 1rem;
-            line-height: 1.6;
-            white-space: break-spaces;
+        @media (max-width: 760px) {
+            .content {
+                flex-direction: column;
+
+                ul {
+                    white-space: break-spaces;
+                }
+
+                p {
+                    padding-left: 0;
+                    margin-left: 0;
+                    border-left: none;
+                    margin-top: 1rem;
+                }
+            }
         }
     }
 </style>
